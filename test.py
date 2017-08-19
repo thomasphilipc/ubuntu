d6876c1f906d5192042eab7b63daa313a096562a
Thomas Philip <thomasphilip.c@gmail.com>
2017-08-19 21:10:35 +0400

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello There! Regards,Thomas , update 9:09 </h1>"
+    return "<h1 style='color:blue'>Hello There! Regards,Thomas , update 9:10 </h1>"
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
