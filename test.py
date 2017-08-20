1f56065f8a8d6e28edf5e6956b963a3d9eb91f72
thomas <thomasphilip.c@gmail.com>
2017-08-20 17:20:52 +0400

    updating some files

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import render_template
+from forms import LoginForm
 application = Flask(__name__)
+application.config.from_object('config')
 
 @application.route("/")
 def hello():
@@ -9,7 +11,25 @@ def hello():
 @application.route("/index")
 def index():
     user = {'nickname': 'Thomas'}  # fake user
-    return  render_template('index.html',title='Home',user=user)
+    posts = [
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html',title='Home',user=user,posts=posts)
+
+@application.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        print(form.username)
+        print(form.password)
+    return render_template('login.html', title='Sign In', form=form)
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
