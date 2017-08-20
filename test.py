0330b84c27e6ab3deb4958902e951bb125062ebc
thomas <thomasphilip.c@gmail.com>
2017-08-20 12:29:16 +0400

    test from laptop

--- test.py
+++ test.py
@@ -5,5 +5,9 @@ application = Flask(__name__)
 def hello():
     return "<h1 style='color:blue'>Hello There! Regards,Thomas , update 9:10 </h1>"
 
+@application.route("/index")
+def index():
+    return "<h1 style='color:blue'>Hello There! You are on the index page </h1>"
+
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
