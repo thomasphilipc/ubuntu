32eef75d345f3266db46df77f4ef115d14bd21fc
Tom <talk2tpc@gmail.com>
2017-09-30 07:57:47 +0400

    website test

--- test.py
+++ test.py
@@ -4,32 +4,21 @@ from forms import LoginForm
 application = Flask(__name__)
 application.config.from_object('config')
 
-@application.route("/")
+@app.route("/")
 def hello():
-    return "<h1> Thomas has forgotten about this website. remind him</h1>"
+    return render_template ("index.html")
 
-@application.route("/index")
-def index():
-    user = {'nickname': 'Thomas'}  # fake user
-    posts = [
-        {
-            'author': {'nickname': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'nickname': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html',title='Home',user=user,posts=posts)
+@app.route("/about")
+def about():
+    return render_template ("about.html")
 
-@application.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        print(form.username)
-        print(form.password)
-    return render_template('login.html', title='Sign In', form=form)
+@app.route("/resume")
+def resume():
+    return render_template ("resume.html")
+
+@app.route("/projects")
+def projects():
+    return render_template ("projects.html")
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
